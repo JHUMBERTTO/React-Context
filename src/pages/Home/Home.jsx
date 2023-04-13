708b7f84f419a13bc66b72e01a92060ddae94537
Jhumberto <jhumbertto@gmail.com>
2023-04-13 16:10:38 -0600

    Song List Components and Function

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import './home.css'
+import SongList from '@/components/SongList'
 
 const Home = () => {
   return (
     <div className='home-container'>
       <div className='izquierdo'>
-        <h2>Lado Izquierdo</h2>
+        <SongList />
       </div>
       <div className='derecho'>
         <h2>Lado  Derecho</h2>

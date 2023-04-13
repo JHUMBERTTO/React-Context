3ad43b6a3616850cbe70a96f4fe426b3df84cf20
Jhumberto <jhumbertto@gmail.com>
2023-04-13 16:46:36 -0600

    Song Provider & Context Logic

---
+++ src/context/SongContext.jsx
@@ -0,0 +1,58 @@
+// Context tiene que ver con el manejo de estados globales en react
+// Eso quiere decir que nosotros podemos compartir las misma informacion entre diferentes niveles de componentes
+
+import { createContext, useState, useEffect, useContext } from 'react'
+import canciones from '@/assets/listaCanciones'
+
+// Para usar context, necesitamos seguir una serie de pasos.
+
+// 1 Creacion del contexto vacio
+
+const SongContext = createContext()
+
+// 2 Creacion del componente proveedor del contexto es decir, maneja de donde se obtiene y como pasa la info.
+
+function SongProvider (props) {
+  const [list, setList] = useState([])// Lista de canciones
+  const [loading, setLoading] = useState(true)// Ya se cargo la info (Loader)
+  const [selectedSong, setSelectedSong] = useState({})
+
+  // simulo llamada a la API de la lista canciones
+
+  useEffect(() => {
+    setTimeout(() => {
+      setList(canciones)
+      setLoading(false)
+    }, 2000)
+  }, [])
+
+  const value = {
+    list,
+    loading,
+    selectedSong,
+    setSelectedSong
+  }
+
+  return (
+    <SongContext.Provider value={value}>
+      {props.children}
+    </SongContext.Provider>
+  )
+}
+
+// 3 Consumidor del contexto, brindara acceso a la data de los componentes
+
+const useSongContext = () => {
+  const context = useContext(SongContext)
+  return context
+}
+
+// 4 Exportar las funciones del Provider y el Consumer para que pueden ser invocados en nuestros componentes
+
+export {
+  SongProvider,
+  useSongContext
+}
+
+// Uso de contexto
+// 5 Ahora debemos de ir a componente superior (por ejemplo Home) y envolver a los componentes que necesitan la informacion del contexto
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,16 +2,19 @@ import React from 'react'
 import './home.css'
 import SongList from '@/components/SongList'
 import SongDetails from '@/components/SongDetails'
+import { SongProvider } from '@/context/SongContext'
 
 const Home = () => {
   return (
     <div className='home-container'>
-      <div className='izquierdo'>
-        <SongList />
-      </div>
-      <div className='derecho'>
-        <SongDetails />
-      </div>
+      <SongProvider>
+        <div className='izquierdo'>
+          <SongList />
+        </div>
+        <div className='derecho'>
+          <SongDetails />
+        </div>
+      </SongProvider>
     </div>
   )
 }
